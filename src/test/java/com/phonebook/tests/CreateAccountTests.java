25eec9e8e526c7af6bb243295a804529c0494f11
arti- <olushka301988@gmail.com>
2025-03-15 15:17:28 +0200

    Add

---
+++ src/test/java/com/phonebook/tests/CreateAccountTests.java
@@ -0,0 +1,22 @@
+package com.phonebook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class CreateAccountTests extends TestBase {
+    @Test
+    public void newUserRegistrationPositiveTest(){
+
+    //click on Login link
+    driver.findElement(By.cssSelector("[href='/login']")).click();
+    //enter email
+
+    //enter password
+
+    //click on Registration button
+
+    //verify SingOut butto is displayed
+    }
+
+
+}
